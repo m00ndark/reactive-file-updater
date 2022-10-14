24490ab2b82aaa6639199351d2f3d68addf8ca13
Mattias Wijkström <mattiawi@axis.com>
2022-10-14 15:35:47 +0200

    Added BOM detection

--- src/ReactiveFileUpdater/Extensions.cs
+++ src/ReactiveFileUpdater/Extensions.cs
@@ -53,12 +53,36 @@ namespace ReactiveFileUpdater
 
 		public static void WriteContent(this TargetFile targetFile, string content, int retries = 3)
 		{
+			UTF8Encoding encoding = new UTF8Encoding(targetFile.HasBom());
+
 			while (true)
 			{
 				try
 				{
-					File.WriteAllText(targetFile.FilePath, content, Encoding.UTF8);
+					File.WriteAllText(targetFile.FilePath, content, encoding);
 					return;
+				}
+				catch (IOException) when (retries-- > 0)
+				{
+					Thread.Sleep(TimeSpan.FromMilliseconds(200));
+				}
+			}
+		}
+
+		private static bool HasBom(this TargetFile targetFile, int retries = 3)
+		{
+			while (true)
+			{
+				try
+				{
+					using (FileStream fileStream = new FileStream(targetFile.FilePath, FileMode.Open, FileAccess.Read))
+					{
+						byte[] bom = new byte[3];
+						int bytesRead = fileStream.Read(bom, 0, bom.Length);
+
+						return bytesRead == bom.Length
+							&& bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf;
+					}
 				}
 				catch (IOException) when (retries-- > 0)
 				{
